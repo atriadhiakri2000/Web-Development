762925c4b3cd9477d81d93915c7fd0280f9cc9ac
Atri Adhikari <atriadhikarinsec@gmail.com>
2020-07-13 17:33:59 +0530

    This is a sample Portfolio Website Made By me

---
+++ Portfolio 1 - Copy/script.js
@@ -0,0 +1,51 @@
+const spans = document.querySelectorAll('h1 span')
+spans.forEach(span => span.addEventListener('mouseover', function(e){
+    span.classList.add('animated', 'rubberband')
+}))
+
+spans.forEach(span => span.addEventListener('mouseout', function(e){
+    span.classList.remove('animated', 'rubberband')
+}))
+
+const htmlBar = document.querySelector('.bar-html')
+const cssBar = document.querySelector('.bar-css')
+const jsBar = document.querySelector('.bar-javascript')
+const reactBar = document.querySelector('.bar-react')
+
+var t1 = new TimelineLite()
+
+t1.fromTo(htmlBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(90% - 6px)`, ease: Power4.easeout})
+.fromTo(cssBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(95% - 6px)`, ease: Power4.easeout})
+.fromTo(jsBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(75% - 6px)`, ease: Power4.easeout})
+.fromTo(reactBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(70% - 6px)`, ease: Power4.easeout})
+
+const contoller = new ScrollMagic.contoller()
+const scene = new ScrollMagic.Scene({
+    triggerElement: '.skills',
+    triggerHook: 0
+})
+.setTween(t1)
+.addTo(contoller)
+
+const showRequiredCategory = event => {
+    const getId = event.getid
+    const links = document.querySelectorAll('.work-category button')
+    for(i=0;i<links.length;i++){
+        if(links[i].hasAttribute('class')){
+            links[i].classList.remove('active')
+        }
+    }
+
+    event.classList.add('active')
+    const getCategory = document.querySelector(`.category-${getId}`)
+    const category = document.querySelectorAll('div[class ^= "category-"]')
+    for(i=0;i<categories.length;i++){
+        if(categories[i].hasAttribute('class')){
+            categories[i].classList.remove('active')
+            categories[i].classList.add('hideCategory')
+        }
+    }
+
+    getCategory.classList.remove('hideCategory')
+    getCategory.classList.add('showCategory')
+}
