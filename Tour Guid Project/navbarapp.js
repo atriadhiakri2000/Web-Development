649a77cc3e17c86b6918fcc8c05cee85cfc35ddb
Atri Adhikari <atriadhikarinsec@gmail.com>
2020-12-07 12:19:49 +0530

    Add files via upload

---
+++ Tour Guid Project/navbarapp.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "225px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
